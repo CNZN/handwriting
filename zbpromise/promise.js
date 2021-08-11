3c2b48a56a9bfd7d030373eb38bcbfd6750b4298
CNZN <2213951433@qq.com>
2021-08-11 19:52:22 +0800

    测试promise/A+

--- zbpromise/promise.js
+++ zbpromise/promise.js
@@ -68,3 +68,15 @@ class MyPromise{
         return promise;
     }
 };
+
+//在手写的promiseXXX.js添加以下代码，其中改成自己定义promise.js名字
+MyPromise.defer = MyPromise.deferred = function(){
+    let dfd = {};
+    dfd.promise = new MyPromise((resolve, reject)=>{
+        dfd.resolve = resolve;
+        dfd.reject = reject;
+    });
+    return dfd;
+}
+
+module.exports =  MyPromise
